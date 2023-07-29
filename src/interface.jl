5e8b620e0d30b70f852f457e094ec48357d032f6
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-07-28 22:32:34 -0300

    Update docstring for isinvertible

--- src/interface.jl
+++ src/interface.jl
@@ -42,7 +42,7 @@ function isrevertible end
     isinvertible(transform)
 
 Tells whether or not the `transform` is invertible, i.e. supports a
-[`inv`](@ref) function. Defaults to `false` for new transform types.
+[`Base.inv`](@ref) function. Defaults to `false` for new transform types.
 
 Transforms can be invertible in the mathematical sense, i.e., there
 exists a one-to-one mapping between input and output spaces.
