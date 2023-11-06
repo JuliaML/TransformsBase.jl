9b20528bafd740c06e3e1d998cf8839de2604dc6
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-11-06 16:11:32 -0300

    Update src/interface.jl

--- src/interface.jl
+++ src/interface.jl
@@ -33,7 +33,8 @@ function isrevertible end
     isinvertible(transform)
 
 Tells whether or not the `transform` is invertible, i.e. whether it
-implements the `inverse` function. Defaults to `false` for new transform types.
+implements the `inverse` function. Defaults to `false` for new transform
+types.
 
 Transforms can be invertible in the mathematical sense, i.e., there
 exists a one-to-one mapping between input and output spaces.
